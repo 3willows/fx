f22de076db154b69eaa9078690144326c450ed96
Jake Lazaroff <yo@jake.nyc>
2023-08-16 10:28:45 -0400

    Change default code

--- src/lib/filter.ts
+++ src/lib/filter.ts
@@ -2,11 +2,17 @@ import { writable } from "svelte/store";
 
 import type { Parameter } from "./Audio";
 
-const CODE =
-  localStorage.getItem("code") ||
-  `for (let i = 0; i < input.length; i++) {\n  output[i] = input[i] * parameters.gain;\n}`;
-const PARAMS =
-  localStorage.getItem("params") || JSON.stringify([{ name: "gain", defaultValue: 1, minValue: 0, maxValue: 1 }]);
+const DEFAULT_CODE = `
+for (let i = 0; i < input.length; i++) {
+  // gain controls the volume! don't set it higher than 1
+  output[i] = input[i] * parameters.gain;
+}
+`.trim();
+
+const DEFAULT_PARAMS = JSON.stringify([{ name: "gain", defaultValue: 1, minValue: 0, maxValue: 1 }]);
+
+const CODE = localStorage.getItem("code") || DEFAULT_CODE;
+const PARAMS = localStorage.getItem("params") || DEFAULT_PARAMS;
 
 export const code = writable(CODE);
 export const params = writable<Parameter[]>(JSON.parse(PARAMS));
